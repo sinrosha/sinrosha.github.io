8d1f7aee3766cffacc4acf70c8f2875ac3840a30
roshan singh <singhroshan209@gmail.com>
2018-06-26 18:19:19 +0530

    We are ready for beta

---
+++ src/js/script.js
@@ -0,0 +1,55 @@
+
+
+function checkFocus() {
+  if ( document.hasFocus() ) {
+    document.title = "Roshan Singh | Front-End Developer and a wannabe designer";
+  } else {
+    document.title = "Hey, I'm here! 👋";
+    var favicon = document.querySelector("link[rel*='icon']");
+    favicon.style.display = "none";
+  }
+}
+
+setInterval( checkFocus, 200 );
+
+$(document).ready(function() {
+  $("#enter").on("click", function() {
+  	var name = $("#name").val();
+  	var greeting = "Welcome " + name;
+  	var charCount = $("#name").val().length;
+
+  	if (charCount > 0 ) {
+        $("#username").text(greeting);
+  		$("body").addClass("loadanswer");
+  	  // $(".question").fadeOut();
+  	  // $("#username").text(greeting);
+  	  // $("body").css("overflow", "hidden");
+  	  // $(".answer").fadeIn().css("display", "flex");
+     //  $("header ul").css("display", "flex").fadeIn();
+
+  	} else {
+  		return
+  	}
+  });
+});
+
+$(document).on("keypress", function (e) {
+   if(e.which == 13) {
+   	$("#enter").click();
+   }
+});
+
+$(document).ready(function() {
+  AOS.init();
+});
+ 
+$(".burger-menu").click(function() {
+  $("header .navbar ul").slideToggle();
+  $(this).toggleClass("change");
+  $("body").toggleClass("overflow");
+  $("header .navbar ul li").click(function() {
+    $("body").removeClass("overflow");
+    $("header .navbar ul").slideUp();
+    $(".burger-menu").removeClass("change");
+  });
+});
