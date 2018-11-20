4e60ca3390206978cba2d473c6b9f104d50f3b14
roshan singh <singhroshan209@gmail.com>
2018-11-20 18:38:05 +0530

    Loader and smooth scroll added

--- src/js/script.js
+++ src/js/script.js
@@ -1,5 +1,4 @@
 
-
 function checkFocus() {
   if ( document.hasFocus() ) {
     document.title = "Roshan Singh | Front-End Developer and a wannabe designer";
@@ -15,7 +14,17 @@ setInterval( checkFocus, 200 );
 
 
 $(document).ready(function() {
+  $(this).scrollTop(0);
   AOS.init();
+  setTimeout( function() { 
+    $(".loader").fadeOut();
+    $("body").removeClass("stop-scroll");
+  }, 7000);
+
+  window.onbeforeunload = function () {
+  window.scrollTo(0, 0);
+}
+
 });
  
 $(".burger-menu").click(function() {
@@ -40,7 +49,7 @@ $(".burger-menu").click(function() {
   .done(function(json) {
 
     const feed = $(json)[0].data;
-    
+    console.log();
     feed.forEach(post => {
        var post_image = post.images.standard_resolution.url;
        $(".posts ul").append("<li><div class='image'><img src='"+ post_image + "'></div</li>");
@@ -57,3 +66,20 @@ $(".burger-menu").click(function() {
      console.log("Request Completed");
   });
 
+
+
+
+
+
+$(document).ready(function(){
+    $('.navbar a').on('click',function (e) {
+        e.preventDefault();
+        var target = this.hash;
+        $target = $(target);
+        $('html, body').stop().animate({
+            'scrollTop':  $target.offset().top 
+        }, 900, 'swing', function () {
+            window.location.hash = target;
+        });
+    });
+});
