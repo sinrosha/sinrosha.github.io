e34a487400bf9956a2fbbff36a549490947d6022
sinrosha <singhroshan209@gmail.com>
2019-09-13 01:34:43 +0530

    New script added

--- strangerandsons/src/js/script.js
+++ strangerandsons/src/js/script.js
@@ -126,7 +126,10 @@ preload([
 ]);
 
 
-// We listen to the resize event
+let vh = window.innerHeight * 0.01;
+// Then we set the value in the --vh custom property to the root of the document
+document.documentElement.style.setProperty('--vh', `${vh}px`);
+
 window.addEventListener('resize', () => {
   // We execute the same script as before
   let vh = window.innerHeight * 0.01;
