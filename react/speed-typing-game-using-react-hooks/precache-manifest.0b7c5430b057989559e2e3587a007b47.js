c63f44f6dcc47091f732041d13c203063b4a047f
sinrosha <singhroshan209@gmail.com>
2020-04-19 21:13:31 +0530

    adding first react project with hooks speed typing game

---
+++ react/speed-typing-game-using-react-hooks/precache-manifest.0b7c5430b057989559e2e3587a007b47.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "85aba65f54bf088ae25d5346a187b865",
+    "url": "/index.html"
+  },
+  {
+    "revision": "bbcf61a3f72ed733e746",
+    "url": "/static/css/main.21a5b3d0.chunk.css"
+  },
+  {
+    "revision": "891cbada375dc12fc9fa",
+    "url": "/static/js/2.01808715.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.01808715.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bbcf61a3f72ed733e746",
+    "url": "/static/js/main.707875a8.chunk.js"
+  },
+  {
+    "revision": "7ee5f0dc6933f44850af",
+    "url": "/static/js/runtime-main.fe0ff93e.js"
+  }
+]);
